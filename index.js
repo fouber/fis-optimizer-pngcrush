5f697d9c800e4a394fb040148fdb6c6a030dca49
xiangshouding <xiangshouding@baidu.com>
2013-07-22 13:37:43 +0800

    update

--- index.js
+++ index.js
@@ -5,9 +5,16 @@
 
 'use strict';
 
-var C = require('node-pngcrush');
-
+try {
+    var C = require('node-pngcrush');
+} catch(e){
+    fis.log.warning('node-pngcrush does not support the version you\'re using node');
+}
 
 module.exports = function(content, file, conf) {
-    return C.option(conf).compress(content);
+    if (C && C.compress) {
+        return C.option(conf).compress(content);
+    } else {
+        return content;
+    }
 };
